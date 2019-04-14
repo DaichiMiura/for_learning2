946e0d2b64ddc790242a3c9a2fa2dc5d15a0ca77
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 17:19:45 +0900

    例外処理

--- js/main.js
+++ js/main.js
@@ -1,37 +1,13 @@
 'use strict';
 {
-    // window.alert('hello'); window省略可
-    // alert('hello');
-
-    // const answer = confirm('Are you sure?');
-    // console.log(answer); // ok -> true no -> false
-
-    // setInterval(関数、ミリ秒)
-    /*
-    let i = 0;
-    const showTime = () => {
-        console.log(new Date());
-        i++;
-        if (i > 2) {
-            clearInterval(timerId);
-        }
-    };
-
-    let timerId = setInterval(showTime, 1000);
-    */
-
-    let i = 0;
-
-    const showTime = () => {
-        console.log(new Date());
-        let timerId = setTimeout(showTime, 1000); // 一度だけ呼び出される
-        i++;
-        if (i > 2) {
-            clearTimeout(timerId);
-        }
-    };
-
-    showTime();
-
     
+    const a = 5;
+
+    try {
+        console.log(a.toUpperCase());
+    } catch(e) {
+        console.log(e.message);
+    }
+    
+    console.log('Finish!');
 }
