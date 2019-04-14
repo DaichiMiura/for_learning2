6240061d6c1afa4d662df5b7b08d65296eb4afcf
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 14:25:56 +0900

    オブジェクトの操作

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 'use strict';
 {
-    const a = [1, 5, 10, 100];
-    for (let i = 0; i < a.length; i++) {
-        console.log(a[i]);
-    }
+    // Object
+    const o = {
+        a: 1,
+        b: 2,
+    };
 
-    a.forEach((item, index) => {
-        console.log(`${index}: ${item}`);
+    console.log(Object.keys(o));    // ['a', 'b']
+    console.log(Object.values(o));  // [1, 2]
+    console.log(Object.entries(o)); // [['a', 2], ['b', 2]]
+
+    Object.keys(o).forEach(key => {
+        console.log(`${key}: ${o[key]}`);
     })
-
-    // mapを使うと全ての要素に同じ処理をすることができる。
-    const b = a.map(item => item * 2);
-    // [2, 10, 20, 200]
-
-    // 真偽値を与える関数を与えて、trueのときだけ要素を取り出せるfilter
-    const c = a.filter(item => item % 2 === 0);
-    // [10, 100]
 }
