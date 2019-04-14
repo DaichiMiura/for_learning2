ba0732f28387d76b5d7bd446652a07036f6fc066
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 13:49:34 +0900

    配列とオブジェクト

--- js/main.js
+++ js/main.js
@@ -1,37 +1,25 @@
 'use strict';
-// 関数
-// 引数にはconstやletはいらない
-function showAd(message = 'AD') { // 仮引数
-    console.log(`---${message}---`);
+{
+    // 配列
+    const scores = [80, 90, 100];
+
+    // オブジェクト(連想配列のようなもん)
+    // コンマ区切りの一つ一つ、名前と値の組をプロパティ(メンバー)という
+    const player = {
+        name: 'taguchi',
+        score: 32,
+    }
+
+    console.log(player['name']);
+    console.log(player.name);
+
+    player.score = 100; // 変更 player[score] = 100 でもOK
+
+    // 値の追加
+    player.email = 'taguchi@gmail.com';
+
+    // 値の削除
+    delete player.score;
+
+    console.log(player);
 }
-
-showAd();        // ---AD---
-showAd('hello'); // ---hello---
-
-// 関数式　無名関数
-const sum = function(a, b, c) {
-    return a + b + c;
-};
-
-const total = sum(1, 2, 3) + sum(4 ,5, 6);
-console.log(total);
-
-// アロー関数
-const sum2 = (a, b, c) => a + b + c;
-console.log(sum2(1, 2, 3));
-
-//引数が１つのときはカッコを省略可能
-const double = a => a * 2;
-console.log(double(12));
-
-// スコープについて
-
-const x = 2; // グローバルスコープ
-
-function f() {
-    const x = 1;
-    console.log(x);
-}
-
-f();           // 1
-console.log(x) // 2
