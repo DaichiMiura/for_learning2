79d4f86d0ad8abb3a61185c8b483d5bdd6ece578
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 13:18:16 +0900

    制御構文

--- js/main.js
+++ js/main.js
@@ -1,18 +1,53 @@
 'use strict';
 
-// データ型
-// 文字列、数値、undefined(未定義)、null(値自体が無い)、真偽値、オブジェクト
+// if
+const score = 60;
 
-// データ型を調べる
-console.log(typeof 'hello');
-console.log(typeof 5);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof 5 < 3);
+if (score >= 75) {
+    console.log('hello');
+} else if (score >= 60) {
+    console.log('good');
+} else {
+    console.log('ok');
+}
 
-// 文字列型の数字同士でも計算可
-// ただし+は文字列の連結になる
-console.log('5' + 3) // 53
+// 短縮形
+score >= 80 ? console.log('great') : console.log('ok');
 
-// 文字列を数値にキャスト
-console.log(parseInt('5, 10') + 3); // 8
+// switch
+const signal = 'green';
+switch (signal) {
+    case 'red':
+        console.log('stop');
+        break;
+    case 'yellow':
+        console.log('caution');
+        break;
+    case 'blue':
+    case 'green':
+        console.log('go');
+        break;
+    default:
+        console.log('wrong signal');
+        break;
+}
+
+// for
+// テンプレートリテラル。バッククォートで囲んだ中では変数や定数の展開可能
+for (let i = 1; i <= 10; i++) {
+    if (i % 3 === 0) {
+        continue;
+    }
+
+    if (i === 8) {
+        break;
+    }
+    console.log(`${i} hello`);
+}
+
+// while
+let hp = 200;
+while (hp > 0) {
+    console.log(`${hp} HP left!`);
+    hp -= 15;
+}
