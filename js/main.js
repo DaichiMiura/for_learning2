f70b0f832d364c442e01db10653b8b4b20b40b7f
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 12:47:27 +0900

    数値計算

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 'use strict';
-    
-console.log('Hello World');
 
-console.log('it\'s me!'); // \でエスケープ
-
-console.log('hell\no wor\tld') // \nは改行、\tはタブ
-
-console.log('hello' + 'hello') // 連結
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3); // 少数は近似される
+console.log(10 % 3);
+console.log(10 ** 3); // 冪乗
