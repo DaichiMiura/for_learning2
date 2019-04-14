0ad8289428fd7e9eb33757dfe3b23851d86ddf8b
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 14:02:33 +0900

    クラス

--- js/main.js
+++ js/main.js
@@ -1,25 +1,42 @@
 'use strict';
 {
-    // 配列
-    const scores = [80, 90, 100];
+    // クラス
+    // メソッドに'function'はつけなくて良い
+    class Player {
+        constructor(name, score) {
+            this.name  = name;
+            this.score = score;
+        }
 
-    // オブジェクト(連想配列のようなもん)
-    // コンマ区切りの一つ一つ、名前と値の組をプロパティ(メンバー)という
-    const player = {
-        name: 'taguchi',
-        score: 32,
+        showInfo() {
+            console.log(`name: ${this.name} score: ${this.score}`)
+        }
+
+        static showVersion() {
+            console.log('player class ver. 1.0');
+        }
     }
 
-    console.log(player['name']);
-    console.log(player.name);
+    // 継承
+    class SoccerPlayer extends Player {
+        constructor(name, score, number) {
+            super(name, score); // 親クラスのコンストラクタを呼び出す
+            this.number = number;
+        }
 
-    player.score = 100; // 変更 player[score] = 100 でもOK
+        kick() {
+            console.log('Gooooooaaaalll');
+        }
+    }
 
-    // 値の追加
-    player.email = 'taguchi@gmail.com';
+    const taguchi = new Player('taguchi', 32);
+    const fkoji   = new Player('fkoji'  , 44);
+    taguchi.showInfo();
+    fkoji.showInfo();
+    Player.showVersion();
 
-    // 値の削除
-    delete player.score;
-
-    console.log(player);
+    const tsubasa = new SoccerPlayer('tsubasa', 99, 10);
+    console.log(tsubasa.number);
+    tsubasa.showInfo();
+    
 }
