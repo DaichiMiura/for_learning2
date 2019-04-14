39a00247d61379cfd29c75d1bdb7e39b55e9da97
DaichiMiura <seed_natural@yahoo.co.jp>
2019-04-14 13:03:19 +0900

    データ型について

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 'use strict';
 
-// 定数
-const price1 = 150;
-console.log(price1 * 140);
+// データ型
+// 文字列、数値、undefined(未定義)、null(値自体が無い)、真偽値、オブジェクト
 
-// 変数
-let price2 = 160;
-price2 = 140;
-console.log(price2 * 140);
+// データ型を調べる
+console.log(typeof 'hello');
+console.log(typeof 5);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof 5 < 3);
 
-price2 += 100;
-price2 -= 100;
-price2 *= 2;
-price2++;
-price2--;
+// 文字列型の数字同士でも計算可
+// ただし+は文字列の連結になる
+console.log('5' + 3) // 53
+
+// 文字列を数値にキャスト
+console.log(parseInt('5, 10') + 3); // 8
